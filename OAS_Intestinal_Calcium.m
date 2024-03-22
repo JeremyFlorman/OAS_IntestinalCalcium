367ae13465d9a16663348ffd87dae90a4855651d
JeremyFlorman <Jeremy.Florman@gmail.com>
2024-03-22 14:11:16 -0400

    added parameter to define "head vs tail" based on intensity

--- OAS_Intestinal_Calcium.m
+++ OAS_Intestinal_Calcium.m
@@ -1,5 +1,5 @@
-fld = 'C:\src\OpenAutoSCope-v2\data\myo-2GFP_autofocusTest'; % Folder containing the data you want to analyze
-serverfolder = 'Z:\OAS\myo-2GFP_autofocusTest';  % upload everything to this location.
+fld = 'C:\src\OpenAutoScope-v2_20240205_1502\data\myo-2HisCL\+HA\240321_zfis178_myo-2HisCl+HA'; % Folder containing the data you want to analyze
+serverfolder = 'Z:\OAS\5-HT\+Food\myo-2HisCL+Food+HA';  % upload everything to this location.
 
 %% settings
 startIndex = 1; % which video to start analysis.
@@ -22,6 +22,7 @@ crop = 1; % num pixels to crop from image edge. set to 0 for no cropping.
 minwormarea = 10000; %lower limit to worm area
 maxwormarea = 20000; % upper limit to worm area
 axSigLen = 200; % how many pixels to use for registering axial signal.
+axSigOrientation = 1; % 0=brighter signal on bottom, 1=brighter signal on top
 
 useautothreshold = 1;% set to 1 to calculate a threshold for each image.
 useadaptivethreshold = 0; % if useautothreshold is set to 0 adaptive thresholding can be used
@@ -287,9 +288,22 @@ for nf =startIndex:length(imgDir)
                         leftMean = mean(tt(1:querryLength),'omitnan');
                         rightMean = mean(tt(length(tt)-querryLength:length(tt)),'omitnan');
 
-                        if leftMean<rightMean
-                            tt = fliplr(tt);
-                            abf = fliplr(abf);
+                        if axSigOrientation == 0
+                            if leftMean>rightMean
+                                temptrace = fliplr(temptrace);
+                                tt = fliplr(tt);
+                                
+                                abf = fliplr(abf);
+                                tempbf = fliplr(tempbf);
+                            end
+                        else
+                            if leftMean<rightMean
+                                temptrace = fliplr(temptrace);
+                                tt = fliplr(tt);
+                                
+                                abf = fliplr(abf);
+                                tempbf = fliplr(tempbf);
+                            end
                         end
 
                         % % % % % % % % % % % % % % % % % % % %
@@ -484,8 +498,9 @@ for nf =startIndex:length(imgDir)
         close(v)
     end
     %% %%%%%%%%%%%%%%%%%%%%% Auto Fix Axial Signal %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    fractionToQuerry = 0.1;
-    autoAxialSignal = autoFixSignal(axialSignal,fractionToQuerry);
+    autoAxialSignal = axialSignal;
+%     fractionToQuerry = 0.1;
+%     autoAxialSignal = autoFixSignal(axialSignal,fractionToQuerry);
 
     %     for ii = 1:length(axialSignal)
     %         left = mean(axialSignal(ii,1:10),'omitnan');
